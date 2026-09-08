3f2e7e4356306f0da4c1bd2af1e87893d2eddc3f
marceloriv <ma.riveracontreras@duocuc.cl>
2026-09-08 17:28:03 -0300

    :recycle: refactor(a11y): optimizar carga lazy de rutas y accesibilidad wcag 2.2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   tenantId: '936612c7-66b8-41ba-a9dd-83f50365f818',
   redirectUri: 'http://localhost:4200',
   apiUrl: 'https://3d2ods6had.execute-api.us-east-1.amazonaws.com',
-  apiEspaciosUrl: 'http://localhost:8082/api/v1',
+  apiEspaciosUrl: 'http://localhost:3000/api/v1/espacios-comunes',
 };
